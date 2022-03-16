d023321e1936bcf7588825435b98acea6fdb83f4
Alejandroxp011 <87455252+Alejandroxp011@users.noreply.github.com>
2022-03-15 21:55:11 -0400

    cambios pequeños

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,7 +12,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <!-- Scripts -->
         @routes
         <script src="{{ mix('js/app.js') }}" defer></script>
